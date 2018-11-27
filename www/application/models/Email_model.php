5fbd55f76204f35d3bc3de11051f37473a6cb552
Reza <rezazramadhan@gmail.com>
2018-11-27 12:08:02 +0700

    sql injection

---
+++ www/application/models/Email_model.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Email_model extends CI_Model {
+
+    public function send_email($email, $subject, $message, $debug = FALSE) {
+    }
+
+}
